fe855f470be39559493fdaa894451b095d836b9e
Guilherme Franco <guilherme.franco@moneri.com.br>
2022-02-28 17:14:26 -0300

    Primeiro commit. Começa a montar a tela de login

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+
+import { LoginComponent } from '../pages/login/login.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
